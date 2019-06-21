13dd94d04112a09f9279681dc1afe26b21e60053
Alex <alexpeterson1@mail.weber.edu>
2019-06-20 19:14:09 -0600

    upload (not working)

--- TestCoreWebApp/TestCoreWebApp/Controllers/ProfScreenController.cs
+++ TestCoreWebApp/TestCoreWebApp/Controllers/ProfScreenController.cs
@@ -8,11 +8,22 @@ using System.IO;
 using System.Collections.ObjectModel;
 using AcesWebApp.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;//
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AcesWebApp.Controllers
 {
     public class ProfScreenController : Controller
     {
+
+        private IHostingEnvironment _hostingEnvironment;
+
+        public ProfScreenController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
         ObservableCollection<Services.ClassRoom> classList = new ObservableCollection<Services.ClassRoom>();
         List<Services.ClassRoom> classList2 = new List<Services.ClassRoom>();
 
@@ -52,5 +63,82 @@ namespace AcesWebApp.Controllers
 
             return View(vm);
         }
+        
+	    [HttpPost]
+        public IActionResult Index(List<IFormFile> files)
+        {
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(files[0].OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadLine());
+            }
+            String Content = result.ToString();
+                //Do something with the files here. 
+                return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadSmallFile(IFormFile file)
+        {
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            if (file.Length > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok();
+        }
+
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            /* long size = files.Sum(f => f.Length);
+
+             // full path to file in temp location
+             var filePath = Path.GetTempFileName();
+
+             foreach (var formFile in files)
+             {
+                 if (formFile.Length > 0)
+                 {
+                     using (var stream = new FileStream(filePath, FileMode.Create))
+                     {
+                         await formFile.CopyToAsync(stream);
+                     }
+                 }
+             }*/
+
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "UploadedFiles");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            //return Ok(new { count = files.Count, size, filePath });
+            return Ok();
+        }
+
+
     }
 }
