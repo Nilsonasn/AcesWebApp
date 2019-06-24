d8b664adb6264524467c24f713b2ddab4dd2e309
RFSnodgrass <rhettsnodgrass@mail.weber.edu>
2019-06-24 12:02:08 -0600

    StudentPage uploads files

--- TestCoreWebApp/TestCoreWebApp/Controllers/StudentPageController.cs
+++ TestCoreWebApp/TestCoreWebApp/Controllers/StudentPageController.cs
@@ -1,17 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AcesWebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcesWebApp.Controllers
 {
     public class StudentPageController : Controller
     {
+        private IHostingEnvironment hostingEnvironment;
+
+        public StudentPageController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         [Route("StudentPage")]
         public IActionResult StudentPage()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Results(StudentPageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+                if (model.StudentUnitTest != null)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "studentCode");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.StudentUnitTest.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.StudentUnitTest.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                }
+
+                if (model.StudentProgramFiles != null)
+                {
+                    foreach (IFormFile studentFile in model.StudentProgramFiles)
+                    {
+                        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "studentCode");
+                        uniqueFileName = Guid.NewGuid().ToString() + "_" + studentFile.FileName;
+                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                        studentFile.CopyTo(new FileStream(filePath, FileMode.Create));
+                    }               
+
+                }
+
+            }            
+
+            return View();
+        }
+
+
     }
 }
---
+++ TestCoreWebApp/TestCoreWebApp/Models/StudentPageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcesWebApp.Models
+{
+    public class StudentPageModel
+    {
+        public List<IFormFile> StudentProgramFiles { get; set; }
+        public IFormFile StudentUnitTest { get; set; }
+    }
+}
